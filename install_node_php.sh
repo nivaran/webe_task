b4b9ee520ba9416272e1a92c2d5b629591b33108
Your Name <you@example.com>
2023-05-13 08:41:59 +0000

    second commit

--- install_node_php.sh
+++ install_node_php.sh
@@ -18,7 +18,11 @@ fi
 
 # Lets Ask user for specific node version to install
 nvm ls-remote
-read -p "Type the specific node version to install with prefiv 'v' from above given version-list : " node_version
+if [ "$#" -eq 2 ];then
+	node_version=$1
+else
+	read -p "Type the specific node version to install with prefiv 'v' from above given version-list : " node_version
+fi
 nvm install $node_version
 #nvm alias default $node_version
 pkill node # Restarting the node to accept the changes
@@ -41,7 +45,10 @@ sudo apt install software-properties-common ca-certificates lsb-release apt-tran
 sudo echo -e "\n" | apt-add-repository ppa:ondrej/php
 sudo sleep 3
 sudo apt-get update
-read -p "enter the version of php , Try version 8.1, enter-  8.1 : " php_version
+if [ "$#" -eq 2 ];then
+	php_version=$2
+else
+	read -p "enter the version of php , Try version 8.1, enter-  8.1 : " php_version
 sudo apt install -y php$php_version 
 echo "Install version is \n $(php -v) \n"
 
